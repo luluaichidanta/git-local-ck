0e267443b70b21c6fb37019d5b008d5858236d8c
lulu <2404671884@qq.com>
2025-04-10 16:02:07 +0800

    lulu-add-回滚

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("我创建的新的分支lulu");    }
+        System.out.println("我创建的新的分支lulu");
+        System.out.println("不需要的代码到了远程仓库");    }
 }
